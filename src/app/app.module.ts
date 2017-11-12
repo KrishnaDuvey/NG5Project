a1ac8a8260e3d5d10c925074ffb8db7ac0632f45
KrishnaDuvey <krishnadubey002@gmail.com>
2017-11-13 00:50:50 +0530

    13-11-2017

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
-
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations'
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
@@ -18,7 +18,7 @@ import { AboutComponent } from './about/about.component';
   imports: [   
     BrowserModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,BrowserAnimationsModule
   ],
   providers: [DataService],
   bootstrap: [AppComponent]
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,10 +11,11 @@ import{ DataService } from '../data.service';
     trigger('goals',[
       transition('*=>*',[
         query(':enter',style({ opacity:0}),{optional:true}),
+
         query(':enter',stagger('300ms',[
           animate('.6s ease-in',keyframes([
             style({opacity:0, transform:'translateY(-75%)',offset:0}),
-            style({opacity:.5, transform:'translateY(35px)',offset:3}),
+            style({opacity:.5, transform:'translateY(35px)',offset:.3}),
             style({opacity:1, transform:'translateY(0)',offset:1}),
           ]))
         ]),{optional:true}),
@@ -22,7 +23,7 @@ import{ DataService } from '../data.service';
         query(':leave',stagger('300ms',[
           animate('.6s ease-in',keyframes([
             style({opacity:1, transform:'translateY(0%)',offset:0}),
-            style({opacity:.5, transform:'translateY(35px)',offset:3}),
+            style({opacity:.5, transform:'translateY(35px)',offset:.3}),
             style({opacity:0, transform:'translateY(-75%)',offset:1}),
           ]))
         ]),{optional:true}),
